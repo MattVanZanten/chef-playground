fd1be1f7ff71660f802486138f549be247098419
mattvanzanten@gmail.com <Matt@archer.local>
2016-03-31 10:16:38 -0700

    adding environment discovery via tags

--- cookbooks/cloudwatch-alarm/recipes/default.rb
+++ cookbooks/cloudwatch-alarm/recipes/default.rb
@@ -6,24 +6,42 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# determine if instance is debian or rh
 if node[:platform_family].include?("debian")
   include_recipe "apt"
 elsif node[:platform_family].include?("rhel")
   include_recipe "yum"
 end
 
+# install awscli if instance is debian
 if node[:platform_family].include?("debian")
   package 'awscli' do
     action :install
   end
 end
 
+# collect region and instance id, set alarm name
 metadata_uri = "http://169.254.169.254/latest/meta-data/"
 az = `curl #{metadata_uri}placement/availability-zone`
 region = `echo #{az} | sed "s/[a-z]$//"`
 instance_id = `curl #{metadata_uri}instance-id`
-cpu_alarm_name = "#{node['alarm']['customer_name']}-#{node['alarm']['namespace']['ec2']}-#{instance_id}-#{node['alarm']['cpu']['cloudwatch']['metric']}"
-status_alarm_name = "#{node['alarm']['customer_name']}-#{node['alarm']['namespace']['ec2']}-#{instance_id}-#{node['alarm']['status']['cloudwatch']['metric']}"
+
+# collect environment info
+tags = `aws ec2 describe-tags --region us-west-2 --filters "Name=resource-id,Values=#{instance_id}" --output text`
+if tags.downcase.include? "prod"
+  environment = "Prod"
+elsif tags.downcase.include? "stag"
+  environment = "Staging"
+elsif tags.downcase.include? "test"
+  environment = "Test"
+elsif tags.downcase.include? "dr"
+  environment = "DR"
+end
+
+# set the name of the alarm
+cpu_alarm_name = "#{node['alarm']['customer_name']}-#{node['alarm']['namespace']['ec2']}-#{environment}-#{instance_id}-#{node['alarm']['cpu']['cloudwatch']['metric']}"
+status_alarm_name = "#{node['alarm']['customer_name']}-#{node['alarm']['namespace']['ec2']}-#{environment}-#{instance_id}-#{node['alarm']['status']['cloudwatch']['metric']}"
 
 execute "create cpu alarm" do
   command "aws cloudwatch put-metric-alarm --alarm-name #{cpu_alarm_name} --alarm-description #{node['alarm']['cpu']['cloudwatch']['description']} --actions-enabled --alarm-actions --metric-name #{node['alarm']['cpu']['cloudwatch']['metric']} --namespace #{node['alarm']['namespace']['ec2']} --statistic Average --period #{node['alarm']['cpu']['cloudwatch']['period']} --threshold #{node['alarm']['cpu']['cloudwatch']['threshold']} --comparison-operator #{node['alarm']['cpu']['cloudwatch']['comparison-operator']} --dimensions \"Name=InstanceId,Value=#{instance_id}\" --evaluation-periods #{node['alarm']['cpu']['cloudwatch']['evaluation-periods']}  --unit Percent --alarm-actions #{node['alarm']['sns']['arn']} --region #{region}"
