2c44f49b8ecdabd6552342d2f387ce3cda3cf436
mattvanzanten@gmail.com <Matt@archer.local>
2016-03-31 10:24:55 -0700

    adding quotes to a string, no way

--- cookbooks/cloudwatch-alarm/recipes/default.rb
+++ cookbooks/cloudwatch-alarm/recipes/default.rb
@@ -27,7 +27,7 @@ az = `curl #{metadata_uri}placement/availability-zone`
 region = `echo #{az} | sed "s/[a-z]$//"`
 instance_id = `curl #{metadata_uri}instance-id`
 
-# collect environment info
+# collect environment info from tags
 tags = `aws ec2 describe-tags --region us-west-2 --filters "Name=resource-id,Values=#{instance_id}" --output text`
 if tags.downcase.include? "prod"
   environment = "Prod"
@@ -44,9 +44,9 @@ cpu_alarm_name = "#{node['alarm']['customer_name']}-#{node['alarm']['namespace']
 status_alarm_name = "#{node['alarm']['customer_name']}-#{node['alarm']['namespace']['ec2']}-#{environment}-#{instance_id}-#{node['alarm']['status']['cloudwatch']['metric']}"
 
 execute "create cpu alarm" do
-  command "aws cloudwatch put-metric-alarm --alarm-name #{cpu_alarm_name} --alarm-description #{node['alarm']['cpu']['cloudwatch']['description']} --actions-enabled --alarm-actions --metric-name #{node['alarm']['cpu']['cloudwatch']['metric']} --namespace #{node['alarm']['namespace']['ec2']} --statistic Average --period #{node['alarm']['cpu']['cloudwatch']['period']} --threshold #{node['alarm']['cpu']['cloudwatch']['threshold']} --comparison-operator #{node['alarm']['cpu']['cloudwatch']['comparison-operator']} --dimensions \"Name=InstanceId,Value=#{instance_id}\" --evaluation-periods #{node['alarm']['cpu']['cloudwatch']['evaluation-periods']}  --unit Percent --alarm-actions #{node['alarm']['sns']['arn']} --region #{region}"
+  command "aws cloudwatch put-metric-alarm --alarm-name #{cpu_alarm_name} --alarm-description '#{node['alarm']['cpu']['cloudwatch']['description']}'' --actions-enabled --alarm-actions --metric-name #{node['alarm']['cpu']['cloudwatch']['metric']} --namespace #{node['alarm']['namespace']['ec2']} --statistic Average --period #{node['alarm']['cpu']['cloudwatch']['period']} --threshold #{node['alarm']['cpu']['cloudwatch']['threshold']} --comparison-operator #{node['alarm']['cpu']['cloudwatch']['comparison-operator']} --dimensions \"Name=InstanceId,Value=#{instance_id}\" --evaluation-periods #{node['alarm']['cpu']['cloudwatch']['evaluation-periods']}  --unit Percent --alarm-actions #{node['alarm']['sns']['arn']} --region #{region}"
 end
 
 execute "create status alarm" do
-  command "aws cloudwatch put-metric-alarm --alarm-name #{status_alarm_name} --alarm-description #{node['alarm']['status']['cloudwatch']['description']} --actions-enabled --alarm-actions --metric-name #{node['alarm']['status']['cloudwatch']['metric']} --namespace #{node['alarm']['namespace']['ec2']} --statistic Average --period #{node['alarm']['status']['cloudwatch']['period']} --threshold #{node['alarm']['status']['cloudwatch']['threshold']} --comparison-operator #{node['alarm']['status']['cloudwatch']['comparison-operator']} --dimensions \"Name=InstanceId,Value=#{instance_id}\" --evaluation-periods #{node['alarm']['status']['cloudwatch']['evaluation-periods']}  --unit Percent --alarm-actions #{node['alarm']['sns']['arn']} --region #{region}"
+  command "aws cloudwatch put-metric-alarm --alarm-name #{status_alarm_name} --alarm-description '#{node['alarm']['status']['cloudwatch']['description']}' --actions-enabled --alarm-actions --metric-name #{node['alarm']['status']['cloudwatch']['metric']} --namespace #{node['alarm']['namespace']['ec2']} --statistic Average --period #{node['alarm']['status']['cloudwatch']['period']} --threshold #{node['alarm']['status']['cloudwatch']['threshold']} --comparison-operator #{node['alarm']['status']['cloudwatch']['comparison-operator']} --dimensions \"Name=InstanceId,Value=#{instance_id}\" --evaluation-periods #{node['alarm']['status']['cloudwatch']['evaluation-periods']}  --unit Percent --alarm-actions #{node['alarm']['sns']['arn']} --region #{region}"
 end
